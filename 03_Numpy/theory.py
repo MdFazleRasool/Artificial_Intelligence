cd3e5cbbb5d2d9e85acc4ad766e56c20404c6655
MdFazleRasool <fazleamity001@gmail.com>
2026-06-09 20:12:48 +0530

    Numpy Notes And Mathematical Functions

---
+++ 03_Numpy/theory.py
@@ -0,0 +1,19 @@
+'''
+Python List vs Numpy Array
+Python lists are versatile and can hold different types of data, but they are not optimized for numerical operations. 
+Numpy arrays, on the other hand, are designed for efficient numerical computations and can only hold elements of the same type.
+Numpy arrays are more memory efficient and faster for numerical operations compared to Python lists.    
+Numpy arrays support element-wise operations, which means you can perform operations on entire arrays without the need for explicit loops.  
+In contrast, with Python lists, you would typically need to use loops or list comprehensions to perform similar operations, which can be less efficient and more verbose.   
+Numpy is homogeneous in nature . 
+''' 
+
+
+'''
+
+1. Numpy arrays are faster (Optimized in C)
+2. Numpy Store data in contiguous memory locations (Better cache performance)
+3. Numpy uses less memory (Homogeneous data types)  
+4. Built in mathematical functions (Vectorized operations)
+
+'''
